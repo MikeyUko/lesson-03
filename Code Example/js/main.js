7685fbc6b1138973893d52e4a420fd22ce3fd603
Michael Anthony <ukomikey@gmail.com>
2023-01-23 18:33:45 -0500

    Played with some CSS and some JS

---
+++ Code Example/js/main.js
@@ -0,0 +1,7 @@
+console.log("Script loaded");
+
+button = document.querySelector("button");
+button.onclick = () => {
+    console.log("YOU CLICKED ME, BITCH!");
+    window.location = "https://google.com";
+};
